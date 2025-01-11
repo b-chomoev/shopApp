7417e92ae1f9ee21ecec48abfb7e2916947f3965
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-01-10 23:24:18 -0600

    Changed a bit config and deleted db.json

--- shop-api/config.ts
+++ shop-api/config.ts
@@ -5,12 +5,7 @@ const rootPath = __dirname;
 const config = {
     rootPath,
     publicPath: path.join(rootPath, 'public'),
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'shop_db',
-    }
+    db: 'mongodb://localhost/shop',
 }
 
 export default config;
