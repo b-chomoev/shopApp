959762eb7055b755e155e160d8a43d6cb2f4c317
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-02-01 15:11:55 -0600

    Displayed products in the table from MUI.

--- shop-frontend/src/features/admin/AdminProductList.tsx
+++ shop-frontend/src/features/admin/AdminProductList.tsx
@@ -5,14 +5,44 @@ import { useAppDispatch, useAppSelector } from '../../app/hooks.ts';
 import { selectUser } from '../users/usersSlice.ts';
 import { useEffect } from 'react';
 import { fetchAdminProducts } from './productsAdminThunk.ts';
+import { selectAdminProductsItems } from './productsAdminSlice.ts';
+import { Product } from '../../types';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const AdminProductList = () => {
   const user = useAppSelector(selectUser);
+  const products = useAppSelector(selectAdminProductsItems);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchAdminProducts());
   }, [dispatch]);
+
+  const columns: GridColDef<Product>[] = [
+    { field: '_id', headerName: 'ID', width: 90 },
+    {
+      field: 'category',
+      headerName: 'Category',
+      width: 150,
+      valueGetter: (_value: string, row: Product) => row.category.title,
+    },
+    {
+      field: 'title',
+      headerName: 'Title',
+      width: 150,
+    },
+    {
+      field: 'description',
+      headerName: 'Description',
+      width: 150,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      width: 150,
+      sortable: true,
+    },
+  ];
 
   return (
     <div>
@@ -22,6 +52,24 @@ const AdminProductList = () => {
             Add product
           </Button>
         )}
+      </Grid>
+
+      <Grid>
+        <DataGrid
+          getRowId={(row) => row._id}
+          rows={products}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
       </Grid>
     </div>
   );
