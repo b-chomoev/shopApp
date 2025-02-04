ad9d511ef9c8ba20a9ffadd09e97cf4b91cc9226
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-02-04 17:45:24 -0600

    Added GoogleOAuthProvider to the main.stx

--- shop-frontend/src/globalConstants.ts
+++ shop-frontend/src/globalConstants.ts
@@ -1,1 +1,2 @@
 export const apiUrl = 'http://localhost:8000';
+export const GOOGLE_CLIENT_ID = '220139344888-cf97q78sucgts6rdp7ar6trsbf4d0ol0.apps.googleusercontent.com';
--- shop-frontend/src/main.tsx
+++ shop-frontend/src/main.tsx
@@ -7,16 +7,20 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { PersistGate } from 'redux-persist/integration/react';
 import { addInterceptors } from './axiosApi.ts';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './globalConstants.ts';
 
 addInterceptors(store);
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <PersistGate persistor={ persistor}>
-      <BrowserRouter>
-        <ToastContainer />
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <Provider store={store}>
+      <PersistGate persistor={ persistor}>
+        <BrowserRouter>
+          <ToastContainer />
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </GoogleOAuthProvider>
 );
